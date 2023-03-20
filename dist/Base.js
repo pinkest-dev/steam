b821229f794c48a3fee6c4cffe77592197775f57
rawr111 <70286254+rawr111@users.noreply.github.com>
2023-03-20 14:08:48 +0300

    (fix) задержка на запрос

--- dist/Base.js
+++ dist/Base.js
@@ -87,6 +87,14 @@ class Base {
                     'User-Agent': `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36`,
                     ...headers
                 },
+                timeout: {
+                    lookup: 100,
+                    connect: 50,
+                    secureConnect: 50,
+                    socket: 1000,
+                    send: 10000,
+                    response: 1000
+                },
                 ...requestOptions
             };
             if (options?.customProxy) {
